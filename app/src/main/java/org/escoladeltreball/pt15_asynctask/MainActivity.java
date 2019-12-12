4fca7c38adf720326c38de1fd38844e25356b22e
manuel <manuelgarcia.mgm@gmail.com>
2019-12-12 20:32:04 +0100

    Finished

--- app/src/main/java/org/escoladeltreball/pt15_asynctask/MainActivity.java
+++ app/src/main/java/org/escoladeltreball/pt15_asynctask/MainActivity.java
@@ -1,8 +1,11 @@
 package org.escoladeltreball.pt15_asynctask;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,9 +14,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    int lCount;
     EditText number;
     TextView result;
 
@@ -22,12 +26,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button fib = findViewById(R.id.fibb);
-        Button fibAsync = findViewById(R.id.fibAsync);
-        number = findViewById(R.id.numero);
-        result = findViewById(R.id.result);
-        fib.setOnClickListener(this);
-        fibAsync.setOnClickListener(this);
+        try {
+
+            Button fib = findViewById(R.id.fibb);
+            Button fibAsync = findViewById(R.id.fibAsync);
+            number = findViewById(R.id.numero);
+            result = findViewById(R.id.result);
+            fib.setOnClickListener(this);
+            fibAsync.setOnClickListener(this);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d("onCreate: ", e.getMessage() + "  " + e.getCause());
+        }
 
 
     }
@@ -37,47 +48,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.fibb:
-                if (number.getText().toString() == null || number.getText().toString() == "") {
+                if (number.getText().toString().equals("")) {
                     Toast.makeText(this, "Inserta un número vàlid", Toast.LENGTH_SHORT).show();
                 } else {
                     try {
+                        long fib = 0;
                         int n = Integer.parseInt(number.getText().toString());
                         for (int i = 0; i <= n; i++) {
-                            Hilos(n);
+                            fib = fibonacci((long) n);
                         }
-
-                        result.setText(String.valueOf((int) n));
+                        Hilos();
+                        result.setText(String.valueOf(fib));
                     } catch (Exception e) {
                         e.printStackTrace();
-                        Log.d("Error", e.getMessage() + " " + e.getStackTrace());
+                        Log.d("Error", e.getMessage() + " " + Arrays.toString(e.getStackTrace()));
                     }
                 }
                 break;
             case R.id.fibAsync:
-                CalculaFibonacci cF = new CalculaFibonacci();
-                cF.execute();
+                if (number.getText().toString().equals("")) {
+                    Toast.makeText(this, "Inserta un número vàlid", Toast.LENGTH_SHORT).show();
+                } else {
+                    try {
+                        CalculaFibonacci cF = new CalculaFibonacci();
+                        int n = Integer.parseInt(number.getText().toString());
+                        cF.execute(n);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Log.d("Error", e.getMessage() + " " + Arrays.toString(e.getStackTrace()));
+                    }
+                }
                 break;
-
-
         }
     }
 
-    private void Hilos(int num) {
+    private void Hilos() {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                //fibonacci(num);
-
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        Toast.makeText(MainActivity.this, "Tasca Thread finalitzada", Toast.LENGTH_LONG).show();
+                        Toast.makeText(MainActivity.this, "Tasca finalitzada", Toast.LENGTH_LONG).show();
                     }
                 });
             }
         }).start();
     }
-
 
     public long fibonacci(long n) {
         if (n == 0)
@@ -85,28 +102,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         else if (n == 1)
             return 1;
         else {
-            lCount = +1;
             return fibonacci(n - 1) + fibonacci(n - 2);
         }
     }
 
 
+    @SuppressLint("StaticFieldLeak")
     public class CalculaFibonacci extends AsyncTask<Integer, Void, Long> {
 
         @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
         protected Long doInBackground(Integer... integers) {
-            //return Long.valueOf(fibonacci(params[0]));
-            return null;
+            int number = integers[0];
+            long fib = 0;
+            for (int i = 0; i < number; i++) {
+                fib = fibonacci(number);
+            }
+            return fib;
+
         }
 
+        @RequiresApi(api = Build.VERSION_CODES.N)
         @Override
         protected void onPostExecute(Long aLong) {
             super.onPostExecute(aLong);
+            String res = String.valueOf(aLong);
+            result.setText(res);
+            Toast.makeText(MainActivity.this, "Tasca finalitzada", Toast.LENGTH_LONG).show();
         }
     }
 }
